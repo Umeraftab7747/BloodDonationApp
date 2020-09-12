c5117fa6cddd5f28481c8d9ee677a4af0b096b57
Umeraftab7747 <umeraftab7747@gmail.com>
2020-09-11 22:34:38 -0700

    flatlist done

--- src/screens/request/MyRequest.js
+++ src/screens/request/MyRequest.js
@@ -17,8 +17,70 @@ import {KeyboardAwareScrollView} from '@codler/react-native-keyboard-aware-scrol
 import {AppButton, NavHeader, AppTextinput} from '../../components';
 import AsyncStorage from '@react-native-community/async-storage';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {AnimatedFlatList, AnimationType} from 'flatlist-intro-animations';
 
 export class MyRequest extends Component {
+  state = {
+    data: [
+      {
+        name: 'Umer',
+        fName: 'Irfan',
+        dob: '17-08-1999',
+        city: 'Sargodha',
+      },
+      {
+        name: 'Umers',
+        fName: 'Irfan',
+        dob: '17-08-1999',
+        city: 'Sargodha',
+      },
+      {
+        name: 'Umes',
+        fName: 'Irfan',
+        dob: '17-08-1999',
+        city: 'Sargodha',
+      },
+      {
+        name: 'Uers',
+        fName: 'Irfan',
+        dob: '17-08-1999',
+        city: 'Sargodha',
+      },
+      {
+        name: 'Urs',
+        fName: 'Irfan',
+        dob: '17-08-1999',
+        city: 'Sargodha',
+      },
+    ],
+  };
+
+  RenderItem = (item) => (
+    <View style={styles.flatlistContainer}>
+      <View style={styles.flatlistItem}>
+        <View style={styles.leftContainer}>
+          {/* img */}
+          <View style={styles.imgcontainer}></View>
+        </View>
+        <View style={styles.RightContainer}>
+          <Text style={styles.nametxt}>NAME</Text>
+          <Text style={styles.addresstxt}>Address</Text>
+          <View style={styles.noConatiner}>
+            <Text style={styles.notxt}>03040506753</Text>
+          </View>
+        </View>
+        <View style={styles.LastContainer}>
+          <View style={styles.requestContainr}>
+            <Text style={styles.requestTxt}>Requested</Text>
+          </View>
+          <View style={styles.circlebLood}>
+            <Text style={styles.circelTxt}>A+</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+
   render() {
     return (
       <View style={styles.Container}>
@@ -38,7 +100,21 @@ export class MyRequest extends Component {
               </TouchableOpacity>
             </View>
           </View>
+          {/* flat */}
         </ImageBackground>
+        <View style={styles.flatlistContainerView}>
+          <AnimatedFlatList
+            contentContainerStyle={{marginTop: -h('1%')}}
+            data={this.state.data}
+            renderItem={({item}) => this.RenderItem(item)}
+            animationType={AnimationType.Dive}
+            keyExtractor={(item) => item.name}
+            animationDuration={1000}
+            focused={true}
+          />
+
+          <View style={styles.frespace}></View>
+        </View>
       </View>
     );
   }
@@ -65,6 +141,7 @@ const styles = StyleSheet.create({
     height: h('15%'),
     flexDirection: 'row',
     borderRadius: h('1%'),
+    marginTop: h('4%'),
   },
   left: {
     // backgroundColor: 'green',
@@ -100,4 +177,102 @@ const styles = StyleSheet.create({
     fontSize: h('4%'),
   },
   Requst: {color: 'white', fontSize: h('2%'), fontWeight: 'bold'},
+  flatlistItem: {
+    backgroundColor: '#fff',
+    width: '90%',
+    height: h('19%'),
+    justifyContent: 'center',
+    flexDirection: 'row',
+    borderRadius: h('1%'),
+  },
+  flatlistContainer: {
+    alignItems: 'center',
+    marginTop: h('2%'),
+  },
+  flatlistContainerView: {
+    backgroundColor: '#E6DDDD',
+    height: '100%',
+  },
+  leftContainer: {
+    // backgroundColor: '#000',
+    width: '30%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  RightContainer: {
+    // backgroundColor: 'red',
+    width: '40%',
+    height: '100%',
+  },
+  imgcontainer: {
+    backgroundColor: '#ea5455',
+    borderRadius: h('100%'),
+    width: '80%',
+    height: '70%',
+  },
+  LastContainer: {
+    // backgroundColor: 'yellow',
+    width: '30%',
+    alignItems: 'flex-end',
+  },
+  nametxt: {
+    color: 'black',
+    fontSize: h('2%'),
+    marginTop: h('3%'),
+  },
+  addresstxt: {
+    color: 'silver',
+    fontSize: h('2%'),
+    // marginTop: h('1%'),
+  },
+  noConatiner: {
+    // backgroundColor: 'red',
+    width: '80%',
+    height: '25%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: h('10%'),
+    marginTop: h('3%'),
+    borderColor: '#ea5455',
+    borderWidth: 1,
+  },
+  requestContainr: {
+    backgroundColor: '#FF215D',
+    width: '80%',
+    height: '25%',
+    justifyContent: 'center',
+    paddingLeft: h('2%'),
+    // borderRadius: h('10%'),
+    marginTop: h('3%'),
+
+    borderTopLeftRadius: h('10%'),
+    borderBottomLeftRadius: h('10%'),
+  },
+  circlebLood: {
+    width: '50%',
+    height: '42%',
+    borderRadius: h('10%'),
+    // backgroundColor: 'red',
+    marginTop: h('2%'),
+    marginRight: h('3%'),
+    borderColor: 'silver',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circelTxt: {
+    color: '#FF215D',
+    fontSize: h('2.5%'),
+  },
+  requestTxt: {
+    color: '#Ffff',
+    fontSize: h('2%'),
+  },
+  frespace: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: h('35.1%'),
+    // marginTop: -h('50%'),
+  },
 });
