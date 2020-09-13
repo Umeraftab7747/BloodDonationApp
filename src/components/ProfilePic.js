78444a4bbfa3d122845b263ff234a2c46c990907
Umeraftab7747 <umeraftab7747@gmail.com>
2020-09-13 16:10:43 -0700

    abc

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,17 +1,69 @@
+/* eslint-disable handle-callback-err */
 import React, {Component} from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import {StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {
   widthPercentageToDP as w,
   heightPercentageToDP as h,
 } from 'react-native-responsive-screen';
 import {Icon} from 'react-native-elements';
 import {Surface} from 'react-native-paper';
+import ImagePicker from 'react-native-image-picker';
+import {instanceAxios, Url} from '../screens/Api';
+import AsyncStorage from '@react-native-community/async-storage';
+
 export class ProfilePic extends Component {
+  state = {img: ''};
+
+  capture = () => {
+    const options = {
+      title: 'Select Avatar',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        this.setState({img: response.uri});
+        const params = {
+          Photo: response.uri,
+        };
+        instanceAxios
+          .post(Url + 'img', params)
+          .then((res) => {
+            const data = res.data;
+            if (data.status === '200') {
+              console.warn(data.msg);
+            } else if (data.status === '404') {
+              alert(data.msg);
+            }
+          })
+          .catch((err) => {
+            console.warn('network error');
+          });
+      }
+    });
+  };
+
   render() {
     return (
-      <TouchableOpacity {...this.props} style={styles.imgbox}>
+      <TouchableOpacity
+        onPress={() => {
+          this.capture();
+        }}
+        {...this.props}
+        style={styles.imgbox}>
         <Surface style={styles.surface}>
-          {/* <Icon name="pluscircle" type="antdesign" color="#5b5656" size={30} /> */}
+          {/* <Image source={{uri: this.state.Photo}} /> */}
         </Surface>
       </TouchableOpacity>
     );
---
+++ src/screens/Api.js
@@ -0,0 +1,3 @@
+const axios = require('axios');
+export const instanceAxios = axios.create({});
+export const Url = 'http://192.168.10.3:3001/';
